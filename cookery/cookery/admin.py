63cc74741b79ee96f9e549cf8b15255c05aa411a
Adam Fast <adam@adamfast.com>
2019-04-21 16:47:19 -0500

    Build out admin classes for a bunch of the models and make them usable for setting up meals and recipes.

--- cookery/cookery/admin.py
+++ cookery/cookery/admin.py
@@ -5,7 +5,54 @@ from django.contrib import admin
 from . import models
 
 
-for model in [
-        models.Recipe, models.Ingredient, models.RecipeIngredient,
-        models.RecipeTag, models.Meal, models.MealTime]:
-    admin.site.register(model)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+admin.site.register(models.Ingredient, IngredientAdmin)
+
+
+class RecipeIngredientAdmin(admin.ModelAdmin):
+    list_display = ('recipe', 'ingredient', 'quantity', 'unit_of_measure', 'preparation_method')
+    list_filter = ('unit_of_measure',)
+
+admin.site.register(models.RecipeIngredient, RecipeIngredientAdmin)
+
+
+class RecipeIngredientInline(admin.TabularInline):
+    model = models.RecipeIngredient
+
+
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'typical_meal', 'difficulty', 'liked', 'slow_cooker_friendly', 'outdoor_cooking_friendly')
+    list_filter = ('typical_meal', 'difficulty', 'liked', 'slow_cooker_friendly', 'outdoor_cooking_friendly')
+    search_fields = ('name',)
+    inlines = (RecipeIngredientInline,)
+
+admin.site.register(models.Recipe, RecipeAdmin)
+
+
+class RecipeTagAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+admin.site.register(models.RecipeTag, RecipeTagAdmin)
+
+
+class MealTimeAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+admin.site.register(models.MealTime, MealTimeAdmin)
+
+
+class MealAdmin(admin.ModelAdmin):
+    list_display = ('date', 'meal', 'recipe_plan')
+    list_filter = ('meal',)
+    date_hierarchy = 'date'
+    prepopulated_fields = {
+        'name': ('date', 'meal'),
+    }
+
+    def recipe_plan(self, instance):
+        return ', '.join([r.name for r in instance.recipes.all()])
+
+admin.site.register(models.Meal, MealAdmin)
